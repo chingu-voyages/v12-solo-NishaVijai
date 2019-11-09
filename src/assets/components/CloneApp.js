f4a8a10cc79df967b3accfab78d5669977150a32
Nisha <nishavijai@gmail.com>
2019-11-09 20:40:48 +0100

    UPDATE MAIN COMPONENT

--- src/assets/components/CloneApp.js
+++ src/assets/components/CloneApp.js
@@ -4,7 +4,6 @@ import '../stylesheets/cloneApp.css';
 import { Header } from '../components/HeaderFolder/Header';
 import { Main } from '../components/MainFolder/Main';
 import { Footer } from '../components/FooterFolder/Footer';
-
 export class CloneApp extends React.Component {
 	render() {
 		return (
--- src/assets/components/MainFolder/MainForside.js
+++ src/assets/components/MainFolder/MainForside.js
@@ -3,10 +3,6 @@ import { MainMere } from '../MainFolder/MainMere';
 
 import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-
-// import React, { Suspense } from 'react';
-
-// const LazyLoadForside = React.lazy(() => import('./ForsideImage'));
 export class MainForside extends React.Component {
 	render() {
 		return (
@@ -17,21 +13,7 @@ export class MainForside extends React.Component {
 				</div>
 
 				<div className="main-forside-pic">
-					{/* <Suspense fallback="Loading...">
-						<LazyLoadForside />
-					</Suspense> */}
 					<a className="link" href="https://vaersgo.dk/tilbud" target="_blank" rel="noopener  noreferrer">
-						{/* <img
-							class="lazy"
-							src="placeholder.jpg"
-							data-src={require('../../../assets/images/MainForside/julepynt_prc_.jpg')}
-							alt=""
-						/> */}
-						{/* <img
-							className="main-image"
-							src={require('../../../assets/images/MainForside/julepynt_prc_.jpg')}
-							alt=""
-						/> */}
 						<img
 							class="lazyload"
 							src="https://media.giphy.com/media/52qtwCtj9OLTi/giphy.gif"
@@ -39,15 +21,6 @@ export class MainForside extends React.Component {
 							data-src={require('../../../assets/images/MainForside/20_prc_paa_alt_banner_1.jpg')}
 							alt=""
 						/>
-						{/* <img
-							data-sizes="auto"
-							data-src={require('../../../assets/images/MainForside/julepynt_prc_.jpg')}
-							data-srcset="{require('../../../assets/images/MainForside/julepynt_prc_.jpg')} 300w,
-							{require('../../../assets/images/MainForside/julepynt_prc_.jpg')} 600w,
-							{require('../../../assets/images/MainForside/julepynt_prc_.jpg')} 900w"
-							class="lazyload"
-							alt=""
-						/> */}
 					</a>
 				</div>
 			</div>
